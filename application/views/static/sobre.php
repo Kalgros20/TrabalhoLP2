8e2a748fbded2704810d255d6589b0f4dc88d51a
Kalgros20 <carlysson.andrey96@gmail.com>
2017-11-02 20:46:17 -0200

    Adiiconando sobres

---
+++ application/views/static/sobre.php
@@ -0,0 +1,9 @@
+                     
+<div class="jumbotron">
+    <h1 class="h1-responsive">Trabalho LP2</h1>
+    <p class="lead">Esse é o sistema de login usado para o trabalho de lp2</p>
+    <hr class="my-2">
+    <p>Apenas para explicar
+    </p>
+    <a class="btn btn-primary btn-lg" role="button">Learn more</a>
+</div>
